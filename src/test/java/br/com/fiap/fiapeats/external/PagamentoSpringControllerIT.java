bd16778f84d99522390700e0aef5ebb7fc92c109
Monica Thamiris Silva Ferreira <monica.tsf@gmail.com>
2025-02-12 23:16:51 -0300

    feat: adicionando configuração sonar

--- src/test/java/br/com/fiap/fiapeats/external/PagamentoSpringControllerIT.java
+++ src/test/java/br/com/fiap/fiapeats/external/PagamentoSpringControllerIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.UUID;
 
